63c43651a33bcd96459101b50b19673b8f1087a0
myslfrohitjha <myslfrohitjha@gmail.com>
2022-08-04 03:21:03 +0530

    updated

--- front-end/online-shopping-app/src/app/components/Product_component/add/add.component.ts
+++ front-end/online-shopping-app/src/app/components/Product_component/add/add.component.ts
@@ -11,11 +11,12 @@ import { ProductService } from 'src/app/services/product.service';
 export class AddProductComponent implements OnInit {
   product: Product;
   category: string[];
-  pname: string[];
+  pname: string = "";
   constructor(private router: Router, private service: ProductService) {
     this.product = new Product(0, '', 0, 0, '');
-    this.category = ['Male', 'Female', 'Unisex'];
-    this.pname = ['Lenovo','Asus','Apple','Sony', 'Samsung', 'Axe Signature', 'Engage', 'Wild Stone', 'Calvin Klein', 'Fogg','Acqua Di Gio','Park Avenue Euphoria','Coco Noir Chanel','Calvin Klein','Gemma Di Paradiso','Creed X','Red Diamond','Evidence']
+    this.category = ['Laptops', 'Phones', 'Camera','Watch', 'TWS', 'Headphones', 'Keyboard','Mouse', 'Speakers'];
+    // this.pname = ['Lenovo','Asus','Apple','Sony', 'Samsung', 'Axe Signature', 'Engage', 'Wild Stone', 'Calvin Klein', 'Fogg','Acqua Di Gio','Park Avenue Euphoria','Coco Noir Chanel','Calvin Klein','Gemma Di Paradiso','Creed X','Red Diamond','Evidence']
+    this.pname = this.pname;
   }
 
   ngOnInit(): void {
